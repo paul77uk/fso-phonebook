48c4eeb38e455294d4a13203b950ccbf429315ac
paul77uk <paul77uk@gmail.com>
2023-11-05 10:55:01 +0000

    completed phonebook ex 3.14

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const cors = require('cors')
+const cors = require("cors");
 
-app.use(cors())
-app.use(express.static('dist'))
+const Person = require("./models/person");
+
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+// mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+// const Person = mongoose.model("Person", personSchema);
+
+app.use(cors());
+app.use(express.static("dist"));
 app.use(
   morgan(function (tokens, req, res) {
     return [
@@ -50,7 +65,9 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get("/api/info", (request, response) => {
@@ -62,14 +79,13 @@ app.get("/api/info", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+  Person.findById(request.params.id).then((person) => {
+    if (person) {
+      response.json(person);
+    } else {
+      response.status(404).end();
+    }
+  });
 });
 
 app.delete("/api/persons/:id", (request, response) => {
@@ -79,27 +95,22 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
-
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.name && !body.number) {
+  if (body.name === undefined && body.number === undefined) {
     return response.status(400).json({
       error: "name and number is missing",
     });
   }
 
-  if (!body.name) {
+  if (body.name === undefined) {
     return response.status(400).json({
       error: "name is missing",
     });
   }
 
-  if (!body.number) {
+  if (body.number === undefined) {
     return response.status(400).json({
       error: "number is missing",
     });
@@ -111,18 +122,25 @@ app.post("/api/persons", (request, response) => {
     });
   }
 
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
 });
 
-const PORT = process.env.PORT || 3001
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
