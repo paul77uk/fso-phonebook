74a52dc314af83da28c721eba50587effe893da7
paul77uk <paul77uk@gmail.com>
2023-11-05 18:12:38 +0000

    completed phonebook ex 3.20

--- index.js
+++ index.js
@@ -37,10 +37,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
-
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
     response.json(persons);
